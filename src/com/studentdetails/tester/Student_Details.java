4cc9606be13cdda808c5a1ac8d9798a8ad86d99f
dhamo180 <dhamodharan.r10@gmail.com>
2022-12-04 12:48:15 +0530

    going to commit student file in github

---
+++ src/com/studentdetails/tester/Student_Details.java
@@ -0,0 +1,24 @@
+package com.studentdetails.tester;
+
+public class Student_Details {
+	public void stdName() {
+		System.out.println("Dinesh");
+	}
+
+	public void stdId() {
+		System.out.println(01);
+	}
+
+	public void stdMarks() {
+		System.out.println("Tamil:35 and English:45 and Maths:55");
+	}
+
+	public static void main(String[] args) {
+		Student_Details std = new Student_Details();
+		std.stdName();
+		std.stdId();
+		std.stdMarks();
+
+	}
+
+}
